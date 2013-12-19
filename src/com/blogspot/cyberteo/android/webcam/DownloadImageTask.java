3ec023ffba05005f46e439a859231d4307188025
matteo <matteo@ubuntu>
2013-12-19 20:17:50 +0100

    Project setup fixed

---
+++ src/com/blogspot/cyberteo/android/webcam/DownloadImageTask.java
@@ -0,0 +1,42 @@
+package com.blogspot.cyberteo.android.webcam;
+
+import java.io.InputStream;
+
+import uk.co.senab.photoview.PhotoViewAttacher;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ImageView;
+
+//Async Task code
+public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+	ImageView bmImage;
+    PhotoViewAttacher mAttacher;
+
+
+	public DownloadImageTask(ImageView bmImage) {
+		this.bmImage = bmImage;
+	}
+
+	protected Bitmap doInBackground(String... urls) {
+		String urldisplay = urls[0];
+		Bitmap mIcon11 = null;
+		try {
+			InputStream in = new java.net.URL(urldisplay).openStream();
+			mIcon11 = BitmapFactory.decodeStream(in);
+		} catch (Exception e) {
+			Log.e("Error", e.getMessage());
+			e.printStackTrace();
+		}
+		return mIcon11;
+
+	}
+	
+	@Override
+	protected void onPostExecute(Bitmap result) {
+		bmImage.setImageBitmap(result);
+		mAttacher = new PhotoViewAttacher(bmImage);
+	}
+}
---
+++ src/com/blogspot/cyberteo/android/webcam/MainActivity.java
@@ -0,0 +1,51 @@
+package com.blogspot.cyberteo.android.webcam;
+
+import com.blogspot.euroteo.webcam.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.ImageView;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity{
+
+	private static final int REFRESH = 0;
+	boolean ZOOM = false;
+	private static final String url = "http://www.foto-webcam.eu/webcam/bruneck/current/720.jpg";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		//Download image with asyncTask, set image with photoViewAttacher
+		new DownloadImageTask((ImageView) findViewById(R.id.imageView1)).execute(url);
+		
+	}
+
+
+	/*
+	 * Menu
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		menu.add(0, REFRESH, 0, "Refresh");
+		return true;
+	}
+
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case (REFRESH):
+			Log.d("tag", "refresh");
+			new DownloadImageTask((ImageView) findViewById(R.id.imageView1)).execute(url);
+			break;
+		}
+		return false;
+	}
+
+}
+
